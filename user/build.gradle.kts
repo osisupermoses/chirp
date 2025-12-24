a6add06423e2dc09b010f0349dd279f8b2087de6
Abiodun Osifowokan <osisuper_moses@yahoo.com>
2025-12-24 18:09:37 +0100

    user-service: password security with bcrypt

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 dependencies {
     implementation(projects.common)
 
+    implementation(libs.spring.boot.starter.security)
     implementation(libs.spring.boot.starter.data.jpa)
     runtimeOnly(libs.postgresql)
 
---
+++ user/src/main/kotlin/com/dervlabs/chirp/infra/config/SecurityBeans.kt
@@ -0,0 +1,13 @@
+package com.dervlabs.chirp.infra.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class SecurityBeans {
+
+    @Bean
+    fun bcryptPasswordEncoder(): PasswordEncoder = BCryptPasswordEncoder(12)
+}
