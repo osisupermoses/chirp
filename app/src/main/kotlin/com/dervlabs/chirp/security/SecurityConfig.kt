133faf91ab2ed3727e5a456ab7c9349102c18697
Abiodun Osifowokan <osisuper_moses@yahoo.com>
2025-12-27 17:33:38 +0100

    user-service: security config additions and migration to kotlin dsl

--- app/src/main/kotlin/com/dervlabs/chirp/security/SecurityConfig.kt
+++ app/src/main/kotlin/com/dervlabs/chirp/security/SecurityConfig.kt
@@ -1,38 +1,44 @@
 package com.dervlabs.chirp.security
 
+import com.dervlabs.chirp.api.config.JwtAuthFilter
 import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class SecurityConfig {
 
     @Bean
-    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
-        return httpSecurity
-            .csrf { it.disable() }
-            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
-            .authorizeHttpRequests { auth ->
-                auth
-                    .requestMatchers("/api/auth/**")
-                    .permitAll()
-                    .dispatcherTypeMatchers(
-                        DispatcherType.ERROR,
-                        DispatcherType.FORWARD
-                    )
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
+    fun filterChain(http: HttpSecurity, jwtAuthFilter: JwtAuthFilter): SecurityFilterChain {
+        http {
+            csrf { disable() }
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
             }
-            .exceptionHandling { configurer ->
-                configurer
-                    .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            authorizeHttpRequests {
+                authorize("/api/auth/**", permitAll)
+                authorize("/api/auth/change-password", authenticated)
+                authorize(
+                    { r ->
+                        r.dispatcherType == DispatcherType.ERROR ||
+                        r.dispatcherType == DispatcherType.FORWARD
+                    },
+                    permitAll
+                )
+                authorize(anyRequest, authenticated)
             }
-            .build()
+            exceptionHandling {
+                authenticationEntryPoint = HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+            }
+        }
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
+        return http.build()
     }
 }
